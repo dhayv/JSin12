e5777392e41cb7738d9a55e096d5645d7c0f2a50
Hayvid <107150556+hellomottoworld@users.noreply.github.com>
2024-05-05 12:25:41 -0400

    Update 30.js

--- 30.js
+++ 30.js
@@ -1,2 +1,23 @@
 // Day 1
-split(seperator, limit) 
+
+// JS syntax
+split(separator, limit) 
+
+  var today = "Today was a very productive day";
+  console.log(today.split(" ", 3)); // Limiting split to 3 elements
+  // Output: [ 'Today', 'was', 'a' ]
+  
+  var tricky = "goshgoshgoshgoshgoshgoshgoshgosh";
+  console.log(tricky.split("sh"));
+  // Output: [ 'go', 'go', 'go', 'go', 'go', 'go', 'go', 'go', '' ]
+
+// Python syntax
+split(separator, maxlimit) 
+
+  today = "Today was a very productive day";
+  print(today.split(" ",3));
+  // Output: [ 'Today', 'was', 'a' ]
+  
+  tricky = "goshgoshgoshgoshgoshgoshgoshgosh";
+  print(tricky.split("sh"));
+  // Output: [ 'go', 'go', 'go', 'go', 'go', 'go', 'go', 'go', '' ]
